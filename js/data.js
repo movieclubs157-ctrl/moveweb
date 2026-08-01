a7c196082929e384a3e34f98c87da4087dab19a7
movieclubs157-ctrl <movieclubs157@gmail.com>
2026-08-01 20:29:55 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,44 +1,12 @@
 const movies = [
     {
         id: 1,
-        name: "Avengers: Endgame",
-        imageUrl: "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?w=500",
-        url: "https://nxsha.space/embed/movie/550",
-        redirectUrl: "https://example.com"
+        name: "Chocolate (2008)",
+        url: "https://nxsha.space/embed/movie/15003",
     },
     {
         id: 2,
-        name: "Batman",
-        imageUrl: "https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?w=500",
-        url: "https://example.com/embed/movie2",
-        redirectUrl: "https://example.com"
+        name: "Beautiful Disaster (2023)",
+        url: "https://nxsha.space/embed/movie/1016121",
     },
-    {
-        id: 3,
-        name: "Spider-Man",
-        imageUrl: "https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=500",
-        url: "https://example.com/embed/movie3",
-        redirectUrl: "https://example.com"
-    },
-    {
-        id: 4,
-        name: "John Wick",
-        imageUrl: "https://images.unsplash.com/photo-1524985069026-dd778a71c7b4?w=500",
-        url: "https://example.com/embed/movie4",
-        redirectUrl: "https://example.com"
-    },
-    {
-        id: 5,
-        name: "Fast X",
-        imageUrl: "https://images.unsplash.com/photo-1478720568477-152d9b164e26?w=500",
-        url: "https://example.com/embed/movie5",
-        redirectUrl: "https://example.com"
-    },
-    {
-        id: 6,
-        name: "Transformers",
-        imageUrl: "https://images.unsplash.com/photo-1460881680858-30d872d5b530?w=500",
-        url: "https://example.com/embed/movie6",
-        redirectUrl: "https://example.com"
-    }
 ];
