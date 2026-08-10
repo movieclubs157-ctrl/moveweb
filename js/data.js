49a0106d531368b702395c6241b8818c0bdcc9e1
movieclubs157-ctrl <movieclubs157@gmail.com>
2026-08-10 12:07:21 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -29,9 +29,4 @@ const movies = [
         name: "Deadly Crush",
         url: "https://nxsha.space/embed/movie/537523",
     },
-    {
-        id: 7,
-        name: "Ehsaas Ep3",
-        url: "https://video.maalcdn.com/Feel/Ehsaas/Ehsaas%20Episode%203.mp4",
-    },
 ];
